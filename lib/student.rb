70fec22ac5dc3b8169b0383355e4b55401fa67ec
Dexter Fitch <fitch.dexter@gmail.com>
2020-09-30 17:30:09 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,31 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
+  def hash_variables(hash)
+    hash.each do |key, val|
+      instance_variable_set("@#{key}", val)
+    end
+  end
+
   def initialize(student_hash)
-    
+    hash_variables(student_hash)
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    hash_variables(attributes_hash)
   end
 
   def self.all
-    
+    @@all
   end
 end
-
